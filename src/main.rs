cae804a2d40ed089e6545824266bc2e0461b71bd
Cristian Scapin <cristian.scapin654@icloud.com>
2026-06-12 22:42:12 +0200

    feat: options and hours

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+use chrono::{prelude::*, Duration};
+use slint::{ModelRc, SharedString, VecModel};
+use std::{cell::RefCell, rc::Rc};
+
 slint::include_modules!();
 
+struct AppState {
+    opts_time: Vec<DateTime<Local>>,
+    opts_text: Vec<SharedString>,
+
+    slint_model: Rc<VecModel<SharedString>>,
+}
+
+fn compute_options() -> (Vec<DateTime<Local>>, Vec<SharedString>) {
+    let now: DateTime<Local> = Local::now();
+    println!("Current time : {}", now);
+
+    let minutes_to_next_quarter = 15 - (now.minute() % 15);
+
+    let first_target = (now + Duration::minutes(minutes_to_next_quarter as i64))
+        .with_second(0)
+        .unwrap()
+        .with_nanosecond(0)
+        .unwrap();
+
+    (0..8).map(|i| {
+        let time = first_target + Duration::minutes(i * 15);
+        let text: SharedString = time.format("%H:%M").to_string().into();
+
+        (time, text)
+    }).unzip()
+}
 
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new().unwrap();
@@ -8,32 +38,83 @@ fn main() -> Result<(), slint::PlatformError> {
     // it runs on the ui thread
     // let timer = slint::Timer::default();
 
+    let (opts_time, opts_text) = compute_options();
+    let slint_model = Rc::new(VecModel::from(opts_text.clone()));
+
+    main_window.set_time_options(ModelRc::from(slint_model.clone()));
+
+    let app_state = Rc::new(RefCell::new(AppState{
+        opts_time,
+        opts_text,
+        slint_model,
+    }));
+
+    // select time
+    main_window.on_select_time({
+        let window_weak = main_window.as_weak();
+        let state_clone = app_state.clone();
+
+        move |index| {
+
+            let window = window_weak.unwrap();
+            let idx: usize = index.try_into().unwrap();
+            let state = state_clone.borrow();
+            let now: DateTime<Local> = Local::now();
+            let target_time = state.opts_time[idx];
+            let remaining_time = target_time - now;
+            let rem_seconds = if remaining_time.num_seconds() > 0 {
+                remaining_time.num_seconds()
+            } else {
+                (Duration::minutes(15) - remaining_time).num_seconds()
+            };
+
+            window.set_timer_time(rem_seconds * 1000);
+            window.set_page(Page::TimerPage);
+            window.set_running_state(true);
+        }
+    });
+
     // start/pause
     main_window.on_start_pause({
         let window_weak = main_window.as_weak();
 
-	move || {
-	    let window = window_weak.unwrap();
+        move || {
+            let window = window_weak.unwrap();
             let now_running = !window.get_running_state();
-            let milliseconds_elapsed = window.get_milliseconds_elapsed();
-            if milliseconds_elapsed > 0 { 
+            let timer_time = window.get_timer_time();
+            if timer_time > 0 {
                 window.set_running_state(now_running);
             } else {
                 let _ = start_with_zero_millis_window.run();
             }
-	}
+        }
     });
 
-    main_window.on_reset({
+    // reset
+    main_window.on_stop({
         let window_weak = main_window.as_weak();
 
         move || {
             let window = window_weak.unwrap();
 
             window.set_running_state(false);
-            window.set_milliseconds_elapsed(0);
+            window.set_timer_time(0);
+            window.set_page(Page::SelectionPage);
         }
     });
+
+    // main_window.on_compute_selection({
+    //     let window_weak = main_window.as_weak();
+
+    //     move || {
+    //         let window = window_weak.unwrap();
+
+    //         window.set_running_state(false);
+    //         window.set_timer_time(0);
+    //         window.set_page(Page::SelectionPage);
+    //     }
+    // });
+
 
     main_window.run()
 }
